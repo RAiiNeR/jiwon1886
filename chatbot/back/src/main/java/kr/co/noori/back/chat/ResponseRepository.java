7d2613fec32efc7948b26c399e0d11c0c955329f
RAiiNeR <147795967+RAiiNeR@users.noreply.github.com>
2024-11-29 18:04:32 +0900

    chatbot update delete

--- chatbot/back/src/main/java/kr/co/noori/back/chat/ResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.noori.back.chat;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResponseRepository extends JpaRepository<Response, Long> {
-    Optional<Response> findByKey(String key); // key로 응답을 조회하는 메서드
-}
