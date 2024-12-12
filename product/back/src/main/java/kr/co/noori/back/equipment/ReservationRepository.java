2e75f610af1a832f9e7675c8c637f4c805332e9f
RAiiNeR <147795967+RAiiNeR@users.noreply.github.com>
2024-12-12 17:25:29 +0900

    delete

--- product/back/src/main/java/kr/co/noori/back/equipment/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.co.noori.back.equipment;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long>{
-    public List<Reservation> findByEquipmentAndStatusOrderByReservedTimeAsc(Equipment equipment, ReservationStatus status);
-}
